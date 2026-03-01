a1ba663586b602e0c46e5662fd26f2a4752ddbc3
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-03-01 16:21:24 +0530

    Maded Basic GetMapping for All Tasks with Service

--- src/main/java/com/app/TodoList/Services/TaskService.java
+++ src/main/java/com/app/TodoList/Services/TaskService.java
@@ -1,7 +1,20 @@
 package com.app.TodoList.Services;
 
+import com.app.TodoList.Entity.Task;
+import com.app.TodoList.Repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
 }
